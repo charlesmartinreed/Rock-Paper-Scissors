e461c2a2ca04ceb855674e998e6a68f12e853c92
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-07 19:15:02 -0600

    Add subtle animation to choice buttons to reflect user win/lose/draw.

--- app.js
+++ app.js
@@ -59,24 +59,48 @@ const scissors_div = document.getElementById('scissors');
 		return choices[randomNumber];
 	}
 
+	// setTimeout(function(){
+	// 	console.log("hello")}, 3000);
+
 	function win(userChoice, computerChoice) {
+		const userChoice_div = document.getElementById(userChoice);
 		userScore++;
-		//update the page
+
+		//update the score
 		userScore_span.innerHTML = userScore;
 		computerScore_span.innerHTML = computerScore;
+
+		//update the result
 		result_p.innerHTML = `${userChoice} beats ${computerChoice}. You win! 🔥`;
+
+		//add the class to the clicked div
+		//classList is an array of classes on that element
+		userChoice_div.classList.add('green-glow');
+		setTimeout(function(){
+			//remove the class after 300 millisecond
+			userChoice_div.classList.remove('green-glow');
+		}, 300);
 	}
 
 	function lose(userChoice, computerChoice) {
+		const userChoice_div = document.getElementById(userChoice);
 		computerScore++;
-
 		computerScore_span.innerHTML = computerScore;
 		userScore_span.innerHTML = userScore;
 		result_p.innerHTML = `${userChoice} is beaten by ${computerChoice}. You lose! 😢`;
+		userChoice_div.classList.add('red-glow');
+		setTimeout(function(){
+			userChoice_div.classList.remove('red-glow');
+		}, 300);
 	}
 
 	function draw(userChoice, computerChoice) {
+		const userChoice_div = document.getElementById(userChoice);
 		result_p.innerHTML = `${userChoice} vs ${computerChoice}? It's a draw!`;
+		userChoice_div.classList.add('gray-glow');
+		setTimeout(function(){
+			userChoice_div.classList.remove('gray-glow');
+		}, 300);
 	}
 
 	main();
